eb3a9e8ee1f021b58695d700c31ec3fe7979114b
SalvatoreCognetta <salvatore.cognetta@gmail.com>
2020-04-01 20:22:07 +0200

    Init commit

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+./bin/yscenetrace tests/01_cornellbox/cornellbox.json -o out/naive/01_cornellbox_512_256.jpg -t naive -s 256 -r 512
+./bin/yscenetrace tests/02_matte/matte.json -o out/naive/02_matte_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/03_metal/metal.json -o out/naive/03_metal_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/04_plastic/plastic.json -o out/naive/04_plastic_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/05_glass/glass.json -o out/naive/05_glass_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/06_opacity/opacity.json -o out/naive/06_opacity_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/07_hair/hair.json -o out/naive/07_hair_720_256.jpg -t naive -s 256 -r 720
+./bin/yscenetrace tests/08_lens/lens.json -o out/naive/08_lens_720_256.jpg -t naive -s 256 -r 720
+
+./bin/yscenetrace tests/01_cornellbox/cornellbox.json -o out/path/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+./bin/yscenetrace tests/02_matte/matte.json -o out/path/02_matte_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/03_metal/metal.json -o out/path/03_metal_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/04_plastic/plastic.json -o out/path/04_plastic_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/05_glass/glass.json -o out/path/05_glass_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/06_opacity/opacity.json -o out/path/06_opacity_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/07_hair/hair.json -o out/path/07_hair_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/08_lens/lens.json -o out/path/08_lens_720_256.jpg -t path -s 256 -r 720
+
+./bin/yscenetrace tests/11_bathroom1/bathroom1.json -o out/path/11_bathroom1_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/12_ecosys/ecosys.json -o out/path/12_ecosys_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/13_bedroom/bedroom.json -o out/path/13_bedroom_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/14_car1/car1.json -o out/path/14_car1_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/15_classroom/classroom.json -o out/path/15_classroom_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/16_coffee/coffee.json -o out/path/16_coffee_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/17_kitchen/kitchen.json -o out/path/17_kitchen_720_256.jpg -t path -s 256 -r 720
